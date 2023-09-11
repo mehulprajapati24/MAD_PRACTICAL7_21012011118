7d5dd61de50747b0b73d028dad3d0b7e5ad18fc3
student <student@gnu.ac.in>
2023-09-11 10:39:27 +0530

    completed

--- app/src/main/java/com/example/mad_practical7_21012011118/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical7_21012011118/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.mad_practical7_21012011118
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val videoView: VideoView = findViewById(R.id.videoViewId)
+        val btn: FloatingActionButton = findViewById(R.id.button)
+
+        btn.setOnClickListener {
+            
+        }
     }
 }
